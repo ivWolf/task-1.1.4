80f6b9ee7c946c669e682f8f1b63a3dd667afb38
ivWolf <volkovairina@list.ru>
2024-06-24 10:34:25 +0300

    задача 1.1.4 до проверки

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
+        Util.getConnection();
+
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Света", "Иванова", (byte) 18);
+        userDao.saveUser("Уля", "Иванова", (byte) 16);
+        userDao.saveUser("Гриша", "Колин", (byte) 19);
+        userDao.saveUser("Андрей", "Пучков", (byte) 20);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
         // реализуйте алгоритм здесь
     }
 }
